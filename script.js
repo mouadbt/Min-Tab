c10065b50911ec279a427f53e6d36c893a6c170b
mouadbt <160415648+mouadbt@users.noreply.github.com>
2025-12-24 00:15:54 +0100

    Refactor: restructure page by editing html/css --phase2: adds buttons icons and finish search input focus style

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-import {icons} from "./icons.json"
-document.addEventListener('click', () => {
-    // ------------ get page elements
-    const DOMelements=[
-        {
-            selectorName:"search-btn",
-            selectorType:"#",
-            selectorEl:"searchBtn",
-            value:"searchIcon"
-        },
-        {
-            selectorName:"settingsIcon",
-            selectorType:"#",
-            selectorEl:"settingsBtn"
-        }
-    ]
-    // ------------ load page elements
-    // load icons
-    DOMelements.map((el)=>{
-        document.querySelector(`${el.selectorType}${el.selectorName}`).innerHTML=el.value;
-    });
+document.addEventListener('DOMContentLoaded', () => {
 
-})
+  loadIcons();
+
+});
+
+// Function that get all icons and load them in the page using createElement
+async function loadIcons() {
+  try {
+    const response = await fetch('./assets/data/icons.json');
+    const icons = await response.json();
+    const parser = new DOMParser();
+
+    for (const el of document.querySelectorAll('[data-icon]')) {
+      const icon = icons[el.dataset.icon];
+      if (icon && icon.content) {
+        const svgString = `
+          <svg viewBox="0 0 24 24" width="100%" height="100%" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            ${icon.content}
+          </svg>
+        `;
+
+        const doc = parser.parseFromString(svgString, "image/svg+xml");
+        el.replaceChildren(doc.documentElement);
+      }
+    }
+  } catch (error) {
+    console.error("Failed to load icons:", error);
+  }
+}

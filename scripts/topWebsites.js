6729cea15c7ac5c91a7f45763170d74439419247
mouadbt <160415648+mouadbt@users.noreply.github.com>
2026-06-05 00:03:21 +0100

    fix: fix typo error

--- scripts/topWebsites.js
+++ scripts/topWebsites.js
@@ -1,10 +1,10 @@
-const topWebsiteListConatiner = document.querySelector("#top-website-list-container");
+const topWebsiteListContainer = document.querySelector("#top-website-list-container");
 const manageTopWebsitesBtn = document.querySelector("#manage-top-websites-btn");
 export function initTopWebsiteLogic() {
     if (manageTopWebsitesBtn) {
         manageTopWebsitesBtn.addEventListener("click", () => {
-            if (topWebsiteListConatiner) {
-                topWebsiteListConatiner.classList.toggle("edit-mode");
+            if (topWebsiteListContainer) {
+                topWebsiteListContainer.classList.toggle("edit-mode");
             }
         });
     }
